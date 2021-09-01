1dd5250367243afe79a07ed74792bb4bd55ab9eb
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-09-01 09:11:27 -0700

    Fix test
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- hack/test.sh
+++ hack/test.sh
@@ -3,9 +3,7 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
 # shellcheck source=/dev/null
 root="$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/../"
-
-readonly name="${1:-linux}"
-shift || true
+readonly root
 
 # Simple no-thrill build tester
 if ! "$root/hack/build.sh" \
@@ -14,7 +12,7 @@ if ! "$root/hack/build.sh" \
 	  --inject date=2021-08-01 \
 	  --inject suite=bullseye \
     --inject platforms=linux/amd64,linux/arm64 \
-  	"$name" "$@"; then
+  	"linux" "$@"; then
   printf >&2 "Failed building\n"
   exit 1
 fi
